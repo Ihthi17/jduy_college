6f6bcddb2e0577dcbdb1d6b19e5afb0d8454dc53
ihthi17 <mohamedihthisham17@gmail.com>
2024-07-24 22:45:01 +0530

    first commit

---
+++ transfer_out.php
@@ -0,0 +1,60 @@
+<?php
+include 'includes/header.php';
+include 'includes/navbar.php';
+include 'includes/sidebar.php';
+?>
+<div class="content-wrapper"> 
+   <!-- main content start -->
+          
+   <div class="container-fluid pt-4 px-4">
+<div class="card">
+  <div class="card-header">
+                       Transfer Out List
+                    </div>
+  <div class="card-body">
+    <div class="col-md-12 text-left">
+                    <button type="button" class="btn btn-primary mr-2" onclick="window.location.href='#'"><i class="fa-solid fa-right-left"></i> Transfer</button>
+                    <button type="button" class="btn btn-info"><i class="fa-solid fa-arrows-rotate"></i> Refresh</button>
+                </div>
+                <br>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Student ID</th>
+          <th scope="col">Transfer ID</th>
+          <th scope="col">Univercity Name</th>
+          <th scope="col">Date</th>
+          <th scope="col">Note</th>
+          <th scope="col">Action</th>
+          
+          </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+
+          </tbody>
+    </table>
+  </div>
+  </div>
+
+   </div>
+</div>
+<?php include 'includes/footer.php';?>
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<?php include 'includes/script.php' ?>
