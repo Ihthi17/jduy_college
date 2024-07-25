a1cd4902d52a9d7a4f8f285d403ef27e920d2b95
ihthi17 <mohamedihthisham17@gmail.com>
2024-07-25 11:34:20 +0530

    changes

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -213,7 +213,7 @@
                                 </a>
                                 <ul class="nav nav-treeview">
                                     <li class="nav-item">
-                                        <a href="#" class="nav-link">
+                                        <a href="create_faculty.php" class="nav-link">
                                             <i class="far fa-circle nav-icon"></i>
                                             <p>Create Faculty</p>
                                         </a>
@@ -230,7 +230,7 @@
                                 </a>
                                 <ul class="nav nav-treeview">
                                     <li class="nav-item">
-                                        <a href="#" class="nav-link">
+                                        <a href="create_program.php" class="nav-link">
                                             <i class="far fa-circle nav-icon"></i>
                                             <p>Create Program</p>
                                         </a>
