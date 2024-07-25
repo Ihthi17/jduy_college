11d44a92118d7a5bd11b77f2970239c4d439bfa9
ihthi17 <mohamedihthisham17@gmail.com>
2024-07-25 21:54:53 +0530

    first commit

---
+++ exam_routine.php
@@ -0,0 +1,92 @@
+<?php
+include 'includes/header.php';
+include 'includes/navbar.php';
+include 'includes/sidebar.php';
+?>
+
+<div class="content-wrapper"> 
+   <!-- main content start -->
+          
+   <div class="container-fluid pt-4 px-4">
+
+<div class="card">
+  <div class="card-header">
+   Exam Routine
+  </div>
+  <div class="card-body">
+   <div class="col-md-12 text-left">
+                    <button type="button" class="btn btn-primary mr-2" ><i class="fa-solid fa-plus"></i> Add New</button>
+                    <button type="button" class="btn btn-info"><i class="fa-solid fa-arrows-rotate"></i> Refresh</button>
+                </div>
+                <br>
+    <div class="row">
+            <div class="col-md-3 col-3">
+                <label for="program">Faculty</label>
+                 <select id="faculty" class="form-control">
+                  <option value="All">All</option>
+                 
+                  </select>
+            </div>
+            <div class="col-md-3 col-3">
+                <label for="status">Program</label>
+                 <select id="program" class="form-control">
+                  <option value="All">All</option>
+                  
+                  </select>
+            </div>
+           <div class="col-md-3 col-3">
+                <label for="status">Module </label>
+                 <select id="program" class="form-control">
+                  <option value="All">All</option>
+                 
+                  </select>
+            </div>
+            <div class="col-md-3 col-3">
+                <label for="status">Semester</label>
+                 <select id="program" class="form-control">
+                  <option value="All">All</option>
+                 
+                  </select>
+            </div>
+                </div>
+                <br>
+                <div class="row">
+            <div class="col-md-3 col-3">
+                <label for="status">Section</label>
+                 <select id="program" class="form-control">
+                  <option value="All">All</option>
+                 
+                  </select>
+            </div>
+             <div class="col-md-3 col-3">
+                <label for="status">Type</label>
+                 <select id="program" class="form-control">
+                  <option value="All">All</option>
+                 
+                  </select>
+            </div>
+            
+           
+              <div class="col-md-3 col-3 d-flex align-items-end">
+            <button class="btn btn-info"><i class="fas fa-search"></i> Filter</button>
+            </div>
+        </div>
+  </div>
+
+ 
+</div>
+<br>
+
+
+   </div>
+</div>
+<?php include 'includes/footer.php';?>
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<?php include 'includes/script.php' ?>
