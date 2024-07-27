b0a444c56a02a8ff3565d38f51fcd65c1799534c
ihthi17 <mohamedihthisham17@gmail.com>
2024-07-27 23:18:37 +0530

    sidebar menu link add

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -653,7 +653,35 @@
                
                    </li>
         </li>
-            
+                      <!--Transcripts-->
+                      <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>
+                               Transcripts
+                                <i class="right fas fa-angle-left"></i>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="semester_marksheet.php" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Semester Marksheet</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="certificate.php" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>
+                                      Certificate
+                                       
+                                    </p>
+                                </a>
+                               
+                            </li>
+                        </ul>
+                      </li>
+                </ul>
 
             </nav>
             <!-- /.sidebar-menu -->
